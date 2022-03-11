e243da5f69d644cedafcba915ef107c198e13867
Nicolas Chatelais <ni.chatelais@gmail.com>
2022-03-11 15:32:34 +0100

    add logo

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -4,6 +4,10 @@ import IdPhoto from "../../assets/img/id.png";
 
 const StyledHero = styled.section`
 	display: flex;
+	justify-content: space-around;
+	img {
+		height: 27rem;
+	}
 `;
 
 export default function Hero() {
